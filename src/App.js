b84c6ee2b86b5894d1b2137aaec3da8059514c28
Shivansh Rastogi <88771326+Shivansh1203@users.noreply.github.com>
2023-05-06 19:59:07 +0530

    push index and app

--- src/App.js
+++ src/App.js
@@ -3,25 +3,19 @@ import Home from './Home'
 import SingleMovie from './SingleMovie'
 import Error from './Error'
 
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import {Routes, Route} from "react-router-dom"
 const App = () => {
   return (
-    <>
-
-      <BrowserRouter>
-      
+    <>   
         <Routes>
 
           <Route path="/" element ={<Home/>}/>
           <Route path="movie/:id" element ={<SingleMovie/>}/>
           <Route path="*" element ={<Error/>}/>
         </Routes>
-      
-      </BrowserRouter>
 
     </>
-    
-
+                
   )
 }
 
