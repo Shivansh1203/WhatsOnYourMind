016b49c9eed5a6e7e88f20cefb0e81c80f53c769
Shivansh Rastogi <88771326+Shivansh1203@users.noreply.github.com>
2023-05-06 20:45:45 +0530

    push

--- src/context.js
+++ src/context.js
@@ -1,8 +1,21 @@
-import React, {useContext} from "react";
+import React, {useContext, useEffect} from "react";
 const AppContext = React.createContext();
 
+const API_URL = `http://www.omdbapi.com/?apikey=8e61bbbb&s=titanic`
 const AppProvider = ({children}) =>{
 
+    const getMovies = async(url) => {
+        try{
+            const res = await fetch(url);
+            const data = res.json();
+            console.log(data);
+        }catch(error){
+            console.log(error)
+        }
+    }
+    useEffect(()=>{
+        getMovies(API_URL);
+    },[]);
     return <AppContext.Provider value="shivansh">{children}</AppContext.Provider>
     
 };
