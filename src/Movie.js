5f5a77c4cdd88b39fc47bb2aefc1a9fb92f94450
Shivansh Rastogi <88771326+Shivansh1203@users.noreply.github.com>
2023-05-06 21:22:52 +0530

    push

--- src/Movie.js
+++ src/Movie.js
@@ -10,9 +10,6 @@ const Movie = () => {
     return <div className="loading">Loading....</div>;
   }
 
-  /* plz subsribe to thapa technical channel 
-          https://www.youtube.com/thapatechnical
-         */
 
   return (
     <>
